8eb9f88edb323ed3436accf12b106b09a3ffa081
Corné Driesprong <cdriesprong@gmail.com>
2018-08-15 18:58:22 +0200

    added uiview extensions

--- CP3Extensions/UIView+subviews.swift
+++ CP3Extensions/UIView+subviews.swift
@@ -29,4 +29,33 @@ public extension UIView {
 
         return subviews
     }
+
+    var screenshot: UIImage {
+
+        UIGraphicsBeginImageContextWithOptions(bounds.size, true, 0.0)
+        if let tableView = self as? UITableView {
+            tableView.superview!.layer.render(in: UIGraphicsGetCurrentContext()!)
+        } else {
+            layer.render(in: UIGraphicsGetCurrentContext()!)
+        }
+        let image = UIGraphicsGetImageFromCurrentImageContext();
+        UIGraphicsEndImageContext();
+
+        return image!
+    }
+}
+
+public protocol UIViewLoading {}
+
+public extension UIView: UIViewLoading {}
+
+public extension UIViewLoading where Self: UIView {
+
+    static func loadFromNib() -> UIView {
+
+        let nibName = "\(self)".characters.split{$0 == "."}.map(String.init).last!
+        let nib = UINib(nibName: nibName, bundle: nil)
+
+        return nib.instantiate(withOwner: self, options: nil).first as! Self
+    }
 }
